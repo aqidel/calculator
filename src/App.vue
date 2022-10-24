fcedb97d578ec449ad69eb024817ee24800de7b0
aqidel <fanofparadox@gmail.com>
2022-10-24 21:04:30 +0300

    buttons added

--- src/App.vue
+++ src/App.vue
@@ -4,16 +4,24 @@
     <div id="display-result-wrap">
       <div id="display-result"></div>
     </div>
+    <div id="buttons-wrap">
+      <ButtonComponent v-for="(value, key) in values" :key="key" :value="value"/>
+    </div>
   </div>
 </template>
 
 <script>
-//import ButtonComponent from './components/ButtonComponent.vue'
+import ButtonComponent from './components/ButtonComponent.vue'
 
 export default {
   name: 'App',
   components: {
-    //ButtonComponent
+    ButtonComponent
+  },
+  data() {
+    return {
+      values: ["C", "%", "DEL", "/", "7", "8", "9", "*", "4", "5", "6", "-", "1", "2", "3", "+", "00", "0", ",", "="]
+    }
   }
 }
 </script>
@@ -28,15 +36,15 @@ export default {
 
   #main-wrap {
     margin-top: 100px;
-    border: 1px solid #000000;
-    width: 400px;
-    height: 600px;
+    width: 300px;
+    height: 550px;
+    box-sizing: border-box;
   }
 
   #display-last-operation {
     width: 100%;
     height: 150px;
-    background-color: #d9d9d9;
+    background-color: #e6e6e6;
   }
 
   #display-result-wrap {
@@ -44,7 +52,7 @@ export default {
     width: 100%;
     height: 50px;
     box-sizing: border-box;
-    background-color: #d9d9d9;
+    background-color: #e6e6e6;
   }
 
   #display-result-active {
@@ -52,20 +60,28 @@ export default {
     width: 100%;
     height: 100%;
     box-sizing: border-box;
-    background-color: #d9d9d9;
+    background-color: #e6e6e6;
   }
 
   #display-result {
     width: 100%;
     height: 100%;
     box-sizing: border-box;
-    background-color: #d9d9d9;
+    background-color: #e6e6e6;
     animation: cursor 1s linear infinite;
   }
 
   @keyframes cursor {
     from { border-right: 2px solid #000000; }
     to {}
+  }
+
+  #buttons-wrap {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    grid-template-rows: repeat(5, 70px);
+    align-items: center;
+    justify-items: center;
   }
 }
 </style>
--- src/components/ButtonComponent.vue
+++ src/components/ButtonComponent.vue
@@ -1,12 +1,45 @@
 <template>
-  <div></div>
+  <div class="cl-button" :style="computedStyle">{{ value }}</div>
 </template>
 
 <script>
 export default {
-  name: 'ButtonComponent'
+  name: 'ButtonComponent',
+  props: {
+    value: String
+  },
+  computed: {
+    computedStyle() {
+      let style = {
+        backgroundColor: "#ffffff",
+        color: "#000000"
+      };
+      if (['0', '00', '1', '2', '3', '4', '5', '6', '7', '8', '9', ',', '%', 'C', 'DEL'].includes(this.value)) {
+        style.backgroundColor = "#ffffff";
+      } else if (['/', '*', '-', '+'].includes(this.value)) {
+        style.backgroundColor = "#e6e6e6";
+      } else if (this.value == '=') {
+        style.backgroundColor = "#0066ff";
+        style.color = "#ffffff";
+      }
+      return style;
+    }
+  }
 }
 </script>
 
-<style>
+<style scoped>
+@media screen and (min-width: 1024px) {
+  @import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+  .cl-button {
+    border-radius: 50%;
+    width: 60px;
+    height: 60px;
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    font-size: 20px;
+    font-family: 'Roboto', sans-serif;
+  }
+}
 </style>
