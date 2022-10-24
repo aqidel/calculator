5e5d9832ed6be9f77b2c67a13e9ab2b3321ed2f5
aqidel <fanofparadox@gmail.com>
2022-10-24 15:23:00 +0300

    display is ready

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,8 @@
 <template>
   <div id="main-wrap">
-    <div id="display-wrap">
-      <div id="display"></div>
+    <div id="display-last-operation"></div>
+    <div id="display-result-wrap">
+      <div id="display-result"></div>
     </div>
   </div>
 </template>
@@ -18,24 +19,53 @@ export default {
 </script>
 
 <style>
-body {
+@media screen and (min-width: 1024px) {
+  body {
   display: flex;
   justify-content: center;
   margin: 0;
-}
+  }
 
-#main-wrap {
-  margin-top: 100px;
-  border: 1px solid #000000;
-  width: 400px;
-  height: 600px;
-}
+  #main-wrap {
+    margin-top: 100px;
+    border: 1px solid #000000;
+    width: 400px;
+    height: 600px;
+  }
 
-#display-wrap {
-  padding-bottom: 20px;
-  width: 100%;
-  height: 200px;
-  box-sizing: border-box;
-  background-color: aquamarine;
+  #display-last-operation {
+    width: 100%;
+    height: 150px;
+    background-color: #d9d9d9;
+  }
+
+  #display-result-wrap {
+    padding: 8px;
+    width: 100%;
+    height: 50px;
+    box-sizing: border-box;
+    background-color: #d9d9d9;
+  }
+
+  #display-result-active {
+    border-right: 2px solid #000000;
+    width: 100%;
+    height: 100%;
+    box-sizing: border-box;
+    background-color: #d9d9d9;
+  }
+
+  #display-result {
+    width: 100%;
+    height: 100%;
+    box-sizing: border-box;
+    background-color: #d9d9d9;
+    animation: cursor 1s linear infinite;
+  }
+
+  @keyframes cursor {
+    from { border-right: 2px solid #000000; }
+    to {}
+  }
 }
 </style>
